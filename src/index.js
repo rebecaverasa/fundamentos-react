69254d21ad25c01a76251898ee6dceacca6a6b56
Rebeca Veras <rebecaverasa@gmail.com>
2022-11-16 23:37:46 -0300

    parameters

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Primeiro from "./components/Primeiro.jsx";
+import ComParametro from "./components/ComParametro.jsx";
 
 ReactDOM.render(
-<Primeiro></Primeiro>, 
-document.getElementById("root")
+  <div>
+    <Primeiro></Primeiro>
+    <ComParametro
+      Titulo="esse eh o titulo"
+      Subtitulo="esse eh o subtitulo"
+    ></ComParametro>
+
+    <ComParametro
+      Titulo="opa"
+      Subtitulo="epa"
+    ></ComParametro>
+  </div>,
+
+  document.getElementById("root")
 );
